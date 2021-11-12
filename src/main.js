19b8ad0800d6b9f21226bae53ec0351b7149ab6a
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-11-11 23:54:17 -0300

    feat: structure of project and installed packages of primevue, axios and vuex

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+import store from "./store";
+
+createApp(App)
+  .use(PrimeVue)
+  .use(store)
+  .mount('#app')
